f6359a2c6752874590c3642a4fd9108d8d7b0959
Ashutosh <ashuma2721@gmail.com>
2019-03-16 14:45:15 +0530

    remove commented code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,5 @@
 import { SHOW_TYPE } from "./types";
 
-export const createTask = task => {
-  if (localStorage.tasks) {
-    let tasksData = JSON.parse(localStorage.getItem("tasks"));
-    tasksData.push(task);
-    localStorage.setItem("tasks", JSON.stringify(tasksData));
-  }
-};
-
 export const showType = type => {
   return {
     type: SHOW_TYPE,
